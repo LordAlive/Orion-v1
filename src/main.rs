8cb0cd2202f69cfbfa8825601a298af6dafca0b3
prasad00019 <laxmiprasad.00019@gmail.com>
2025-08-22 23:03:24 +0530

    Json deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod errors;
-mod vm; 
-use errors::VMResult; 
-use vm::instruction::Instruction as I; 
-use vm::VM; 
-fn main()->VMResult<()>{
-    let inst = vec![I::PUSH((10)),I::PUSH((20)),I::ADD,I::PRINT]; 
-    let result = VM::new(inst).run(); 
-    result
+mod vm;
+mod loader;
+use errors::VMResult;
+use vm::VM;
+use vm::instruction::Instruction as I;
+fn main() -> VMResult<()> {
+    let args: Vec<String> = std::env::args().collect();
+    let program = if args.len() > 1 {
+        loader::load_program_from_json(&args[1])?
+    } else {
+        vec![I::PUSH(5), I::PUSH(10), I::ADD, I::PRINT, I::HALT]
+    };
+
+    VM::with_gas_limit(program, 10_000).run()
 }
--- src/vm/instruction.rs
+++ src/vm/instruction.rs
@@ -1,17 +1,28 @@
-
-#[derive(Debug,Clone)]
-pub enum Instruction{
+#[derive(Debug, Clone)]
+pub enum Instruction {
     PUSH(usize),
     // POP,
     MSTORE,
     MLOAD,
-    SSTORE,
-    SLOAD,
+    SSTORE(String),
+    SLOAD(String),
     ADD,
     SUB,
     MUL,
     DIV,
     HALT,
     DESTRUCT,
-    PRINT
+    PRINT,
+}
+pub struct GasCost;
+impl GasCost {
+    pub const HALT: u64 = 1;
+    pub const PUSH: u64 = 2;
+    pub const DIV: u64 = 1;
+    pub const ADD: u64 = 3;
+    pub const SUB: u64 = 3;
+    pub const MUL: u64 = 5;
+    pub const SSTORE: u64 = 20;
+    pub const SLOAD: u64 = 10;
+    pub const PRINT: u64 = 2;
 }
